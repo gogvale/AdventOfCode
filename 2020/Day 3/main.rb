ddf6fde7d9b9bedd73ca1e174c21797ed0223ebf
Gabriel Vale <gogvale2011@gmail.com>
2020-12-03 00:53:51 -0600

    renaming vars

--- 2020/Day 3/main.rb
+++ 2020/Day 3/main.rb
@@ -1,9 +1,9 @@
-input = File.open("input.txt").readlines.map{|line| line.chomp}
+$input = File.open("input.txt").readlines.map(&:chomp)
 
-def slope_trees(right,below,input)
+def slope_trees(right,below)
     current = 0
     total_trees = 0
-    input.each_with_index do |i,idx|
+    $input.each_with_index do |i,idx|
         next if idx % below != 0 or idx == 0
         current += right
         total_trees += 1 if i[current%i.size] == "#"
@@ -12,8 +12,8 @@ def slope_trees(right,below,input)
 end
 
 # Exercise 1
-puts slope_trees(3,1,input)
+puts slope_trees(3,1)
 
 # Exercise 2
-steps = [[1,1],[3,1],[5,1],[7,1],[1,2]]
-puts steps.reduce(1){|total, i| total * slope_trees(i.first,i.last,input)}
+slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]]
+puts slopes.reduce(1){|total, i| total * slope_trees(i.first,i.last)}
