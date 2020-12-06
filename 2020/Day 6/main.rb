225c9c2c9bc0ba194b9b384dc4206730a97efa02
Gabriel Vale <gogvale2011@gmail.com>
2020-12-06 01:00:41 -0600

    correct line break

--- 2020/Day 6/main.rb
+++ 2020/Day 6/main.rb
@@ -2,17 +2,14 @@ input = File.open("input.txt").readlines.map(&:chomp)
 
 i = input.map { |i| i.empty? ? "$" : i }
 
-any =  i.join("")
-        .split("$")
+any =  i.join("").split("$")
         .map {|i| i.split("").uniq.join.length }
         .sum()
 
-all =  i.join(" ")
-        .split("$")
+all =  i.join(" ").split("$")
         .map{|i| i.strip.split.uniq}
         .map{ |i| i.join.split("").uniq
-        .map{|char|  i.map {|i| i.include? char}.reduce(&:&) }
-        .count(true) }
+        .map{|char| i.map {|i| i.include? char}.reduce(&:&) }.count(true) }
         .sum()
 
 puts any # 6947
