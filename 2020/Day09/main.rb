1b7ad9071095596bdb9f11cef493c087c181e167
Gabriel Oliveira <gogvale2011@gmail.com>
2022-11-15 20:01:41 -0600

    Finished 09/2020 pt2

--- 2020/Day09/main.rb
+++ 2020/Day09/main.rb
@@ -1,12 +1,28 @@
 values = File.open('input.txt', 'r').to_a.map { _1.chomp.to_i }
 @index = 25
+
 until @index == values.size
   preamble = values[@index - 25..@index]
   value = values[@index]
+  @invalid = value
   unless preamble.permutation(2).map(&:sum).include?(value)
-    puts value
+    puts @invalid
     break
   end
+
   @index += 1
 end
+
+(0..values.length).each do |i|
+  (1..values.length).each do |j|
+    next unless j - i > 1
+
+    inner_values = values[i..j]
+    if inner_values.sum == @invalid
+      puts inner_values.min + inner_values.max
+      break
+    end
+  end
+end
+
 
