87f19fd29a455dce3ef186dc842d2106555a7fd3
Gabriel Oliveira <gogvale2011@gmail.com>
2022-12-05 09:33:59 -0600

    Final trimming

--- 2022/Day 5/main.rb
+++ 2022/Day 5/main.rb
@@ -1,20 +1,16 @@
 # frozen_string_literal: true
 
-values, instructions = File.read("#{File.dirname(__FILE__)}/input.txt")
-                           .split("\n\n")
-                           .map { _1.split("\n") }
+file = File.read("#{File.dirname(__FILE__)}/input.txt")
+values, instructions = file.split("\n\n").map { _1.split("\n") }
 silver = values.each_with_object([]) do |value, arr|
-  arr << value.chars.each_slice(4).to_a.map(&:join).map do
-    _1.scan(/\[(.)\]/).first&.first
-  end.reverse
-end.transpose.map(&:reverse).map(&:compact).reverse
+  arr << value.chars.each_slice(4).map { _1.join.scan(/\[(.)\]/).first }.reverse
+end.transpose.map { _1.reverse.compact }.reverse
 gold = silver.map(&:dup)
-
 instructions.each do |instruction|
   number, from, to = instruction.scan(/(\d+)/).map(&:first).map(&:to_i)
   number.times { silver[to - 1] << silver[from - 1].pop }
   gold[to - 1] += gold[from - 1].pop(number)
 end
 
-puts silver.map { _1.last }.join
-puts gold.map { _1.last }.join
+puts silver.map(&:last).join
+puts gold.map(&:last).join
