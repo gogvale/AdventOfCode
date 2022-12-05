2cdaae4b0869c9ce885c483be41cee4d991a8634
Gabriel Oliveira <gogvale2011@gmail.com>
2022-12-05 09:20:18 -0600

    Simplify code

--- 2022/Day 5/main.rb
+++ 2022/Day 5/main.rb
@@ -1,26 +1,18 @@
 # frozen_string_literal: true
 
-file = File.open("#{File.dirname(__FILE__)}/input.txt").readlines
-index = file.index { _1.match? /^\n$/ }
-instructions = file[index + 1..]
-@values = file[0...index]
-
-crates = @values.each_with_object([]) do |value, arr|
+values, instructions = File.open("#{File.dirname(__FILE__)}/input.txt").read.split("\n\n").map{_1.split("\n")}
+silver = values.each_with_object([]) do |value, arr|
   arr << value.chars.each_slice(4).to_a.map(&:join).map do
     _1.scan(/\[(.)\]/).first&.first
   end.reverse
 end.transpose.map(&:reverse).map(&:compact).reverse
+gold = silver.map(&:dup)
 
-def parse_instruction(instruction)
-  instruction.scan(/(\d+)/).map(&:first).map(&:to_i)
+instructions.each do |instruction|
+  number, from, to = instruction.scan(/(\d+)/).map(&:first).map(&:to_i)
+  number.times { silver[to - 1] << silver[from - 1].pop }
+  gold[to - 1] += gold[from - 1].pop(number)
 end
 
-puts (instructions.each_with_object(crates.map(&:dup)) do |instruction, crates|
-  number, from, to = parse_instruction(instruction)
-  number.times { crates[to - 1] << crates[from - 1].pop }
-end.map { _1.last }.join)
-
-puts (instructions.each_with_object(crates.map(&:dup)) do |instruction, crates|
-  number, from, to = parse_instruction(instruction)
-  crates[to - 1] += crates[from - 1].pop(number)
-end.map { _1.last }.join)
+puts silver.map { _1.last }.join
+puts gold.map { _1.last }.join
