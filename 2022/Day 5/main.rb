8134845a3daa3d6e67232c8f95031fded8bfbf6e
Gabriel Oliveira <gogvale2011@gmail.com>
2022-12-05 08:56:09 -0600

    Fix memory issue

--- 2022/Day 5/main.rb
+++ 2022/Day 5/main.rb
@@ -5,24 +5,22 @@ index = file.index { _1.match? /^\n$/ }
 instructions = file[index + 1..]
 @values = file[0...index]
 
-def crates
-  @values.each_with_object([]) do |value, crates|
-    crates << value.chars.each_slice(4).to_a.map(&:join).map do
-      _1.scan(/\[(.)\]/).first&.first
-    end.reverse
-  end.transpose.map(&:reverse).map(&:compact).reverse
-end
+crates = @values.each_with_object([]) do |value, arr|
+  arr << value.chars.each_slice(4).to_a.map(&:join).map do
+    _1.scan(/\[(.)\]/).first&.first
+  end.reverse
+end.transpose.map(&:reverse).map(&:compact).reverse
 
 def parse_instruction(instruction)
   instruction.scan(/(\d+)/).map(&:first).map(&:to_i)
 end
 
-silver = instructions.each_with_object(crates) do |instruction, crates|
+silver = instructions.each_with_object(crates.map(&:dup)) do |instruction, crates|
   number, from, to = parse_instruction(instruction)
   number.times { crates[to - 1] << crates[from - 1].pop }
 end.map { _1.last }.join
 
-gold = instructions.each_with_object(crates) do |instruction, crates|
+gold = instructions.each_with_object(crates.map(&:dup)) do |instruction, crates|
   number, from, to = parse_instruction(instruction)
   crates[to - 1] += crates[from - 1].pop(number)
 end.map { _1.last }.join
