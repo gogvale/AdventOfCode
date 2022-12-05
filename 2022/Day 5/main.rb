2d197ab8f5ba2d706b023036626b4a7ce301f4ab
Gabriel Oliveira <gogvale2011@gmail.com>
2022-12-05 10:05:25 -0600

    Improve speed

--- 2022/Day 5/main.rb
+++ 2022/Day 5/main.rb
@@ -3,8 +3,8 @@
 file = File.read("#{File.dirname(__FILE__)}/input.txt")
 values, instructions = file.split("\n\n").map { _1.split("\n") }
 silver = values.each_with_object([]) do |value, arr|
-  arr << value.chars.each_slice(4).map { _1.join.scan(/\[(.)\]/).first }.reverse
-end.transpose.map { _1.reverse.compact }.reverse
+  arr << value.chars.each_slice(4).map { _1.join.scan(/\[(.)\]/).first }
+end.transpose.map { _1.compact.reverse }
 gold = silver.map(&:dup)
 instructions.each do |instruction|
   number, from, to = instruction.scan(/(\d+)/).map(&:first).map(&:to_i)
