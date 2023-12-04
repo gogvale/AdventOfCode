11e25becb2ba6f62dba2f2fc224b11cdac05a387
Gabriel Oliveira <gogvale2011@gmail.com>
2023-12-04 11:33:07 -0600

    Optimize performance

--- 2023/04.rb
+++ 2023/04.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 input = IO.readlines("#{File.dirname(__FILE__)}/input.txt", chomp: true).map do |line|
-  line.split(': ').last.split(' | ').map { _1.scan(/\d+/).map(&:to_i) }
+  line.split(': ').last.split(' | ').map { _1.scan(/\d+/).map(&:to_i) }.reduce(:&).count
 end
-silver = input.sum { (2 ** ((_1 & _2).size - 1)).to_i }
+silver = input.sum { (2 ** (_1 - 1)).to_i }
 scratchcards = (1..input.size).map { [_1, 1] }.to_h
 input.each_with_index do |line, i|
-  (i + 2..line.reduce(:&).size + i + 1).each do |j|
+  (i + 2..line + i + 1).each do |j|
     scratchcards[j] += scratchcards[i + 1] if scratchcards.keys.include? j
   end
 end
