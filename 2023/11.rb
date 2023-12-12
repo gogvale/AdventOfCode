015ab182bcf5649920886f371631c70439a34369
Gabriel Oliveira <gabriel.oliveira@axented.com>
2023-12-11 18:52:02 -0600

    gold day 11

--- 2023/11.rb
+++ 2023/11.rb
@@ -2,17 +2,25 @@
 
 input = IO.readlines("#{File.dirname(__FILE__)}/input.txt", chomp: true)
 # Duplicate empty lines
-input.each_with_index.filter_map { _2 if /\A\.+\Z/.match? _1 }.each_with_index do |i, j|
-  input.insert(i + j, input[i + j])
-end
-(0...input.first.length).filter { |i| input.all? { _1[i] == '.' } }.each_with_index do |i, j|
-  input.map { _1.insert(i + j, '.') }
-end
+rows = input.each_with_index.filter_map { _2 if /\A\.+\Z/.match? _1 }
+cols = (0...input.first.length).filter { |i| input.all? { _1[i] == '.' } }
+
 coords = input.each_with_index.map do |line, index|
   line.enum_for(:scan, /#/).map { [index, Regexp.last_match.begin(0)] }
 end.flatten(1)
+
 silver = coords.combination(2).map do |c1, c2|
-  c1.zip(c2).sum { (_2 - _1).abs }
+  c1.zip(c2).each_with_index.sum do |values, index|
+    a, b = values.sort
+    b - a + ((a..b).to_a & [rows, cols][index]).count
+  end
 end.sum
 
-puts silver
+gold = coords.combination(2).map do |c1, c2|
+  c1.zip(c2).each_with_index.sum do |values, index|
+    a, b = values.sort
+    b - a + ((a..b).to_a & [rows, cols][index]).count * (1_000_000-1)
+  end
+end.sum
+
+puts silver, gold
